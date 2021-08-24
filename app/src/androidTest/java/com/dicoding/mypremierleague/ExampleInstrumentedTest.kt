ca6c9a65e7de580494514a8495b08353f26b320a
5751mn <edi.eastfield99@gmail.com>
2021-08-24 23:06:35 +0800

    gradle update

--- app/src/androidTest/java/com/dicoding/mypremierleague/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/dicoding/mypremierleague/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.dicoding.mypremierleague", appContext.packageName)
+        assertEquals("com.dicoding.micrometer", appContext.packageName)
     }
 }
